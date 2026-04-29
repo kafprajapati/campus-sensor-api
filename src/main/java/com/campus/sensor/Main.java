039c5849c375536a47f0e822de6d32ee602322fe
kafin <kafin@LAPTOP-VLMV3A4P>
2026-04-29 12:18:14 +0545

    Part 1 - server bootstrap, @ApplicationPath and discovery endpoint GET /api/v1

---
+++ src/main/java/com/campus/sensor/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.campus.sensor;
+
+/**
+ *
+ * @author kafin
+ */
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import java.net.URI;
+
+public class Main {
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    public static void main(String[] args) throws Exception {
+        ResourceConfig config = new ResourceConfig()
+                .packages("com.campus.sensor");
+        HttpServer server = GrizzlyHttpServerFactory
+                .createHttpServer(URI.create(BASE_URI), config);
+        System.out.println("===============================================");
+        System.out.println("  Campus Sensor API is running!");
+        System.out.println("  URL: " + BASE_URI + "api/v1");
+        System.out.println("  Press ENTER to stop the server...");
+        System.out.println("===============================================");
+        System.in.read();
+        server.stop();
+    }
+}
---
+++ src/main/java/com/campus/sensor/resource/DiscoveryResource.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.campus.sensor.resource;
+
+/**
+ *
+ * @author kafin
+ */
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public class DiscoveryResource {
+
+    @GET
+    public Response discover() {
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("name", "Campus Sensor API");
+        response.put("version", "1.0");
+        response.put("description", "REST API for managing campus rooms and IoT sensors");
+        Map<String, String> contact = new LinkedHashMap<>();
+        contact.put("name", "API Administrator");
+        contact.put("email", "admin@campus.ac.uk");
+        response.put("contact", contact);
+        Map<String, String> resources = new LinkedHashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+        response.put("resources", resources);
+        return Response.ok(response).build();
+    }
+}
